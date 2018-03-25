ebbc876fb071910b5663657b99b36a63db4c37b3
mamedpoo <32448088+mamedpoo@users.noreply.github.com>
2018-03-26 07:17:51 +0800

    membuat class, attribut dan behavior jam tangan

---
+++ src/praktek01/mamed/JamTangan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek01.mamed;
+
+/**
+ *
+ * @author Akhyat
+ */
+public class JamTangan {
+    String TaliJam;
+    String JenisJam;
+    String Merk;
+    String Bahan;
+    String ThnPmbuatan;
+    
+    void MenampilkanWaktu(){
+         System.out.println("TaliJam                 :"+TaliJam);
+         System.out.println("JenisJam                :"+JenisJam);
+         System.out.println("Merk                    :"+Merk);
+         System.out.println("ThnPmbuatan             :"+ThnPmbuatan);
+    }
+    
+}
